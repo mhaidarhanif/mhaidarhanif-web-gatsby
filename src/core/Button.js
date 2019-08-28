ec3c3dfc621f1204764649a00635180f5404b30f
M Haidar Hanif <git@mhaidarhanif.com>
2019-08-29 04:21:00 +0700

    Style Button

--- src/core/Button.js
+++ src/core/Button.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import styled from '@emotion/styled'
 
-const Button = props => {
-  return <button>{props.children}</button>
+import colors from '../styles/colors.json'
+
+const StyledButton = styled.button`
+  border-radius: 10px;
+  border: none;
+  cursor: pointer;
+  font-family: 'Fira Sans', sans-serif;
+  font-size: 20px;
+  font-weight: bold;
+  padding: 10px 20px;
+  text-transform: uppercase;
+  color: ${props => props.color || colors.black.a};
+  background-color: ${props => props.backgroundColor || colors.white.a};
+`
+
+const Button = ({ children, color, backgroundColor }) => {
+  return (
+    <StyledButton color={color} backgroundColor={backgroundColor}>
+      {children}
+    </StyledButton>
+  )
 }
 
 export default Button
