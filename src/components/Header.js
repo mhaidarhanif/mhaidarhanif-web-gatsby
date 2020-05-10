52c9f411d34f8cb00d6dac7de15a098f73055246
M Haidar Hanif <git@mhaidarhanif.com>
2020-05-10 11:03:57 +0800

    Implement header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,47 @@
 import { h } from 'preact'
 import { Link } from 'preact-router/match'
+import styled from '@xstyled/emotion'
+
+const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const HeaderLinkContainer = styled.div`
+  padding: 1rem;
+`
+
+const LogoIcon = styled.img``
+
+const LogoText = styled.h1`
+  color: #f2c94c;
+  letter-spacing: 0.1em;
+  font-family: 'Playfair Display SC', serif;
+`
+
+const MenuIcon = styled.img`
+  padding-right: 15px;
+`
 
 const Header = () => (
-  <header>
+  <HeaderContainer>
+    <HeaderLinkContainer>
+      <Link href='/'>
+        <LogoIcon src='/assets/mhaidarhanif-icon.svg' alt='Haidar Icon' />
+      </Link>
+    </HeaderLinkContainer>
+
     <Link href='/'>
-      <img src='/assets/mhaidarhanif-icon.svg' alt='Icon' />
+      <LogoText>M Haidar Hanif</LogoText>
     </Link>
-    <h1>M Haidar Hanif</h1>
-  </header>
+
+    <HeaderLinkContainer>
+      <Link href='/'>
+        <MenuIcon src='/assets/menu-icon.svg' alt='Menu Icon' />
+      </Link>
+    </HeaderLinkContainer>
+  </HeaderContainer>
 )
 
 export default Header
