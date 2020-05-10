380276650caca0fe4bbb4b5519a815d642b066a7
M Haidar Hanif <git@mhaidarhanif.com>
2020-05-10 12:52:52 +0800

    Improve layout style

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,6 +4,8 @@ import styled from '@xstyled/emotion'
 const LayoutContainer = styled.main`
   width: 960px;
   margin: 0 auto;
+  display: flex;
+  flex-direction: column;
 `
 
 const Layout = ({ children }) => <LayoutContainer>{children}</LayoutContainer>
