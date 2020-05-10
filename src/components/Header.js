5e379b18c74ac83be8701c9b1cc4d38c53f78ddc
M Haidar Hanif <git@mhaidarhanif.com>
2020-05-10 15:40:55 +0800

    Improve header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,47 +1,105 @@
 import { h } from 'preact'
 import { Link } from 'preact-router/match'
-import styled from '@xstyled/emotion'
+import { breakpoints } from '@xstyled/system'
+import styled, { css } from '@xstyled/emotion'
 
-const HeaderContainer = styled.header`
+const Containers = styled.nav`
+  display: block;
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  z-index: 100;
+  padding: 0;
+`
+
+const Row = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  padding: 10px;
+  background-color: black;
+  background-color: rgba(0, 0, 0, 0.8);
+  backdrop-filter: saturate(200%) blur(5px);
 `
 
-const HeaderLinkContainer = styled.div`
-  padding: 1rem;
+const Column = styled.div`
+  display: flex;
+  align-items: center;
 `
 
-const LogoIcon = styled.img``
+const LogoLink = styled(Link)`
+  display: flex;
+  align-items: center;
+`
 
-const LogoText = styled.h1`
-  color: #f2c94c;
+const LogoIcon = styled.img`
+  height: 30px;
+`
+
+const LogoText = styled.span`
+  color: primary;
+  font-size: 1.2em;
+  font-weight: 900;
   letter-spacing: 0.1em;
-  font-family: 'Playfair Display SC', serif;
+  padding-left: 10px;
+`
+
+const NavigationLinks = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  text-transform: uppercase;
+  margin: 0;
+  li {
+    margin: 0 10px;
+    a {
+      color: white;
+    }
+  }
+  ${breakpoints({
+    xs: css`
+      justify-content: space-between;
+      display: none;
+    `,
+    md: css`
+      display: flex;
+    `,
+  })}
 `
 
 const MenuIcon = styled.img`
-  padding-right: 15px;
+  height: 10px;
+  padding: 0 10px;
 `
 
 const Header = () => (
-  <HeaderContainer>
-    <HeaderLinkContainer>
-      <Link href='/'>
-        <LogoIcon src='/assets/mhaidarhanif-icon.svg' alt='Haidar Icon' />
-      </Link>
-    </HeaderLinkContainer>
+  <Containers>
+    <Row>
+      <Column>
+        <LogoLink href='/'>
+          <LogoIcon src='/assets/mhaidarhanif-icon.svg' alt='Logo Icon' />
+          <LogoText href='/'>M Haidar Hanif</LogoText>
+        </LogoLink>
+      </Column>
+      <Column>
+        <NavigationLinks>
+          <li>
+            <Link href='/about'>About</Link>
+          </li>
+          <li>
+            <Link href='/contact'>Contact</Link>
+          </li>
+          <li>
+            <Link href='/projects'>Projects</Link>
+          </li>
+        </NavigationLinks>
 
-    <Link href='/'>
-      <LogoText>M Haidar Hanif</LogoText>
-    </Link>
-
-    <HeaderLinkContainer>
-      <Link href='/'>
-        <MenuIcon src='/assets/menu-icon.svg' alt='Menu Icon' />
-      </Link>
-    </HeaderLinkContainer>
-  </HeaderContainer>
+        <Link href='/'>
+          <MenuIcon src='/assets/menu-icon.svg' alt='Menu Icon' />
+        </Link>
+      </Column>
+    </Row>
+  </Containers>
 )
 
 export default Header
