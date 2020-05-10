51dde99e09dd957e4ca412d23a93b47cd0977cd3
M Haidar Hanif <git@mhaidarhanif.com>
2020-05-10 12:39:57 +0800

    Implement section social

--- src/components/SectionSocial.js
+++ src/components/SectionSocial.js
@@ -1,13 +1,42 @@
 import { h } from 'preact'
+import styled from '@xstyled/emotion'
+
+import socials from '../data/socials.json'
+
+const SectionSocialContainers = styled.section`
+  margin: 60px 0;
+  display: flex;
+  justify-content: center;
+`
+
+const SocialItems = styled.ul`
+  display: flex;
+  padding: 0;
+  color: #f2c94c;
+  list-style-type: none;
+  text-transform: uppercase;
+  letter-spacing: 0.1em;
+  li {
+    margin: 0 15px;
+  }
+`
+
+const SocialItem = styled.li``
 
 const SectionSocial = () => (
-  <section>
-    <ul>
-      <li>
-        <a href='https://example.com'>Item</a>
-      </li>
-    </ul>
-  </section>
+  <SectionSocialContainers>
+    <SocialItems>
+      {socials.map(({ name, url }, index) => {
+        return (
+          <SocialItem key={index}>
+            <a href={url} target='_blank'>
+              {name}
+            </a>
+          </SocialItem>
+        )
+      })}
+    </SocialItems>
+  </SectionSocialContainers>
 )
 
 export default SectionSocial
