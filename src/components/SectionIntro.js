9a75863bbaa73e0d10cf89b6fc044e8f39fb7437
M Haidar Hanif <git@mhaidarhanif.com>
2020-05-10 12:53:20 +0800

    Implement section intro

--- src/components/SectionIntro.js
+++ src/components/SectionIntro.js
@@ -1,25 +1,48 @@
 import { h } from 'preact'
+import styled from '@xstyled/emotion'
+
+import LinkButtons from './LinkButtons'
+import LinkButton from './LinkButton'
+
+const SectionIntroContainer = styled.section`
+  margin: 120px 0;
+`
+
+const SectionIntroRow = styled.div`
+  margin: 15px 0;
+`
+
+const IntroTitle = styled.h2`
+  margin: 0;
+  font-size: 48px;
+`
+
+const IntroParagraph = styled.p`
+  max-width: 600px;
+`
+
+const Topics = styled.div``
 
 const SectionIntro = () => (
-  <section>
-    <div>
-      <h2>Solving world problems, together</h2>
-      <p>
+  <SectionIntroContainer>
+    <SectionIntroRow>
+      <IntroTitle>Solving world problems, together</IntroTitle>
+      <IntroParagraph>
         Earth and human have many problems. Including ours. We just have to get
         more people to solve them. If you are one of the people who care to make
         a difference, let’s collaborate together.
-      </p>
-      <a href='/collaborate'>Collaborate with Haidar</a>
-    </div>
+      </IntroParagraph>
+      <LinkButtons>
+        <LinkButton href='/collaborate' variant='primary'>
+          Collaborate with Haidar
+        </LinkButton>
+      </LinkButtons>
+    </SectionIntroRow>
 
-    <div>
-      <ul>
-        <li>
-          <img src='' alt='' /> <span>Item</span>
-        </li>
-      </ul>
-    </div>
-  </section>
+    <SectionIntroRow>
+      <Topics></Topics>
+    </SectionIntroRow>
+  </SectionIntroContainer>
 )
 
 export default SectionIntro
