39a219b9de2047b7d0827bfeb3343368f71e62ef
M Haidar Hanif <git@mhaidarhanif.com>
2020-05-10 16:05:21 +0800

    Improve section social

--- src/components/SectionSocial.js
+++ src/components/SectionSocial.js
@@ -4,20 +4,22 @@ import styled from '@xstyled/emotion'
 import socials from '../data/socials.json'
 
 const SectionSocialContainers = styled.section`
-  margin: 60px 0;
+  margin: 50px 0;
   display: flex;
   justify-content: center;
 `
 
 const SocialItems = styled.ul`
   display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
   padding: 0;
   color: #f2c94c;
   list-style-type: none;
   text-transform: uppercase;
   letter-spacing: 0.1em;
   li {
-    margin: 0 15px;
+    margin: 10px;
   }
 `
 
