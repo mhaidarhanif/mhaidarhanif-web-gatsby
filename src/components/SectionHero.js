294092fe7778a5cae136a60992c4c4c945fc5538
M Haidar Hanif <git@mhaidarhanif.com>
2020-05-10 12:53:08 +0800

    Improve section hero

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -1,11 +1,12 @@
 import { h } from 'preact'
 import styled from '@xstyled/emotion'
 
+import LinkButtons from './LinkButtons'
 import LinkButton from './LinkButton'
 
 const SectionHeroContainer = styled.section`
   display: flex;
-  margin: 50px 0;
+  margin: 60px 0;
 `
 
 const SectionHeroText = styled.div`
@@ -35,19 +36,7 @@ const HeroTitle = styled.h1`
 
 const HeroTexts = styled.div``
 
-const Paragraph = styled.p`
-  margin: 15px 0;
-  font-size: 18px;
-  line-height: 30px;
-`
-
-const LinkButtons = styled.div`
-  display: flex;
-  margin-top: 15px;
-  a:nth-of-type(n + 2) {
-    margin: 0 15px;
-  }
-`
+const HeroParagraph = styled.p``
 
 const HeroPhoto = styled.img`
   border: 5px solid #222222;
@@ -67,13 +56,13 @@ const SectionHero = () => (
       </HeroTitle>
 
       <HeroTexts>
-        <Paragraph>
+        <HeroParagraph>
           Haidar is a seasoned tech educator and engineer who mentor people in
           software engineering, web development, and industrial career. Beyond
           that, he’s exploring various experiments to solve world problems.
           Especially helping people’s career in the software industry. Normally
           he doesn’t talk in a 3rd person view.
-        </Paragraph>
+        </HeroParagraph>
 
         <LinkButtons>
           <LinkButton href='/projects' variant='primary'>
