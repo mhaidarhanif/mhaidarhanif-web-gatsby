8c1dc38b83b405ed443318c39d7df17e7f85cbbe
M Haidar Hanif <git@mhaidarhanif.com>
2020-05-10 15:40:45 +0800

    Move header and footer into layout

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import { h, Component } from 'preact'
 import { Router } from 'preact-router'
 import { ThemeProvider } from '@xstyled/emotion'
 
-import Header from './components/Header'
-import Footer from './components/Footer'
-
 import Home from './pages/home'
 
 const theme = {
   colors: {
+    black: '#000000',
+    white: '#ffffff',
     primary: '#f2c94c',
     secondary: '#222222',
   },
@@ -27,13 +26,9 @@ export default class App extends Component {
     return (
       <div id='app'>
         <ThemeProvider theme={theme}>
-          <Header />
-
           <Router onChange={this.handleRoute}>
             <Home path='/' />
           </Router>
-
-          <Footer />
         </ThemeProvider>
       </div>
     )
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,32 @@
 import { h } from 'preact'
-import styled from '@xstyled/emotion'
+import { breakpoints } from '@xstyled/system'
+import styled, { css } from '@xstyled/emotion'
+
+import Header from './Header'
+import Footer from './Footer'
 
 const LayoutContainer = styled.main`
-  width: 960px;
   margin: 0 auto;
   display: flex;
   flex-direction: column;
+  ${breakpoints({
+    xs: css`
+      padding: 20px;
+    `,
+    lg: css`
+      width: 960px;
+    `,
+  })}
 `
 
-const Layout = ({ children }) => <LayoutContainer>{children}</LayoutContainer>
+const Layout = ({ children }) => (
+  <LayoutContainer>
+    <Header />
+
+    {children}
+
+    <Footer />
+  </LayoutContainer>
+)
 
 export default Layout
