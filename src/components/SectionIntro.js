6008101b77a3c3785540d50f3fccc8d4e575fc77
M Haidar Hanif <git@mhaidarhanif.com>
2020-05-10 16:06:08 +0800

    Improve section intro naming

--- src/components/SectionIntro.js
+++ src/components/SectionIntro.js
@@ -1,48 +1,41 @@
 import { h } from 'preact'
 import styled from '@xstyled/emotion'
 
+import Title from './Title'
+import Paragraph from './Paragraph'
 import LinkButtons from './LinkButtons'
 import LinkButton from './LinkButton'
 
-const SectionIntroContainer = styled.section`
-  margin: 120px 0;
+const Container = styled.section`
+  margin: 100px 0;
 `
 
-const SectionIntroRow = styled.div`
-  margin: 15px 0;
-`
-
-const IntroTitle = styled.h2`
-  margin: 0;
-  font-size: 48px;
-`
-
-const IntroParagraph = styled.p`
-  max-width: 600px;
+const Row = styled.div`
+  margin: 50px 0;
 `
 
 const Topics = styled.div``
 
 const SectionIntro = () => (
-  <SectionIntroContainer>
-    <SectionIntroRow>
-      <IntroTitle>Solving world problems, together</IntroTitle>
-      <IntroParagraph>
+  <Container>
+    <Row>
+      <Title>Solving world problems, together</Title>
+      <Paragraph>
         Earth and human have many problems. Including ours. We just have to get
         more people to solve them. If you are one of the people who care to make
         a difference, let’s collaborate together.
-      </IntroParagraph>
+      </Paragraph>
       <LinkButtons>
         <LinkButton href='/collaborate' variant='primary'>
           Collaborate with Haidar
         </LinkButton>
       </LinkButtons>
-    </SectionIntroRow>
+    </Row>
 
-    <SectionIntroRow>
+    <Row>
       <Topics></Topics>
-    </SectionIntroRow>
-  </SectionIntroContainer>
+    </Row>
+  </Container>
 )
 
 export default SectionIntro
