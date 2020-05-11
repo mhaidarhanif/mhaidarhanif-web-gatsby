eab03223239710b10cc2c6777afcc05e3af5947d
M Haidar Hanif <git@mhaidarhanif.com>
2020-05-11 23:05:44 +0800

    Update about page content

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -11,11 +11,31 @@ const AboutPage = (props: PageProps) => (
 
     <SectionContent title="About Haidar">
       <p>
-        Haidar is a seasoned tech educator and engineer who mentor people in
-        software engineering, web development, and industrial career. Beyond
-        that, he’s exploring various experiments to solve world problems.
-        Especially helping people’s career in the software industry. Normally he
-        doesn’t talk in a 3rd person view.
+        Haidar is an educator, engineer, explorer, designer, innovator, and
+        consultant. An avid remote worker and digital nomad.
+      </p>
+      <p>
+        He's building an education venture that supports distant learning and
+        remote work named Azobu (<a href="https://azobu.com">azobu.com</a>).
+        Currently teaching online private mentorship as well. Furthermore, he's
+        developing a highly-skilled professionals community named High Skill
+        Masters (<a href="https://highskillmasters.com">highskillmasters.com</a>
+        ). Previously, he was also founding a coding school named Impact Byte (
+        <a href="https://impactbyte.com/about">impactbyte.com</a>) and
+        contributing to a coding school named Hacktiv8 (
+        <a href="https://hackti8.com">hacktiv8.com</a>).
+      </p>
+      <p>
+        His specialties in the last 5 years are often focused on developing,
+        integrating, and teaching software technologies. Especially full-stack
+        web application development, design, and architecture around various
+        open-source toolsets, JavaScript (ES) and TypeScript (TS) along with
+        Node.js, Express.js, REST/GraphQL/WebSocket API, jQuery/Zepto,
+        React/Vue/Angular, Redux, MongoDB, MySQL/MariaDB, PostgreSQL, Redis,
+        Jest/Mocha/Chai, Agile/Lean Software Development with Scrum/Kanban,
+        system administration with Linux/POSIX tooling, Git source control on
+        GitHub/GitLab/Bitbucket, UI/UX design with Figma, also other programming
+        languages such as Python, Ruby, Java, Golang, and others.
       </p>
     </SectionContent>
   </Layout>
