edb3a2342f3b432e1a20680e30f389c08161a983
M Haidar Hanif <git@mhaidarhanif.com>
2020-05-12 03:57:06 +0800

    Extract placeholders into a data file

---
+++ src/data/form.js
@@ -0,0 +1,23 @@
+export const contact = {
+  placeholders: {
+    name: `Tim Berners-Lee`,
+    title: `The Next Web`,
+    email: `tim@www.org`,
+    intentions: [
+      { value: "random", text: "Random Message" },
+      { value: "thanks", text: "Thank You Message" },
+      { value: "collaboration", text: "Collaboration Offer" },
+      { value: "project", text: "Project Offer" },
+      { value: "job", text: "Job Offer" },
+      { value: "help", text: "Asking for Help" },
+      { value: "other", text: "Other" },
+    ],
+    message: `Hello Haidar,
+    
+20 years ago, I invented the World Wide Web. For my next project, I'm building a web for open, linked data that could do for numbers what the Web did for words, pictures, video. So we can unlock our data and reframe the way we use it together.
+    
+Watch my video here: https://ted.com/talks/tim_berners_lee_the_next_web 
+
+Let's collaborate!`,
+  },
+}
