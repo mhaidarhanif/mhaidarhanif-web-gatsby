3e0dbfa0c26b948877aaa2b9fff5f1e6aa7a0d73
M Haidar Hanif <git@mhaidarhanif.com>
2020-05-11 21:50:11 +0800

    Use section content in about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,14 +1,25 @@
 import React from "react"
-import { PageProps, Link } from "gatsby"
+import { PageProps } from "gatsby"
+import { breakpoints } from "@xstyled/system"
+import styled, { css } from "@xstyled/emotion"
 
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
+import SectionContent from "../components/SectionContent"
 
 const AboutPage = (props: PageProps) => (
   <Layout>
     <SEO title="About" />
 
-    <h1>About Haidar</h1>
+    <SectionContent title="About Haidar">
+      <p>
+        Haidar is a seasoned tech educator and engineer who mentor people in
+        software engineering, web development, and industrial career. Beyond
+        that, he’s exploring various experiments to solve world problems.
+        Especially helping people’s career in the software industry. Normally he
+        doesn’t talk in a 3rd person view.
+      </p>
+    </SectionContent>
   </Layout>
 )
 
