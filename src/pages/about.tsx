659f46b584bad566f6f39742ab82ce50eb67fc4e
M Haidar Hanif <git@mhaidarhanif.com>
2020-07-13 21:35:57 +0700

    Improve about page content

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,57 +1,83 @@
 import React from "react"
-import { PageProps } from "gatsby"
+import { PageProps, Link } from "gatsby"
+import styled from "@xstyled/emotion"
 
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 import SectionContent from "../components/SectionContent"
 import Anchor from "../components/Anchor"
+import SpecialtyLinks from "../components/SpecialtyLinks"
+
+import HaidarAvatar from "../components/HaidarAvatar"
+
+const Avatar = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
 
 const AboutPage = (props: PageProps) => (
   <Layout>
     <SEO title="About" />
 
     <SectionContent title="About Haidar">
-      <p>
-        Haidar is an educator, engineer, explorer, designer, innovator,
-        consultant, and mentor. An avid remote worker and digital nomad.
-      </p>
-      <p>
-        He is now focusing to develop an education venture that supports distant
-        learning and remote work named{" "}
-        <Anchor href="https://azobu.com">Azobu</Anchor>, which helps people to
-        start and grow their career in the software industry. Currently doing{" "}
-        <Anchor href="https://azobu.com/mentorship">mentorship</Anchor> there as
-        well. Furthermore, he's developing a highly-skilled professionals
-        community named{" "}
-        <Anchor href="https://highskillmasters.com">High Skill Masters</Anchor>.
-        Previously, he was also founding a coding school named{" "}
-        <Anchor href="https://impactbyte.com/about">Impact Byte</Anchor> and
-        contributing to a coding school named{" "}
-        <Anchor href="https://hacktiv8.com">Hacktiv8</Anchor>. Few years ago, he
-        was a software engineer in{" "}
-        <Anchor href="https://agnium.co.id">Agnium</Anchor> and{" "}
-        <Anchor href="https://amp.asia">AMPlified</Anchor>. Along the way, he
-        was also doing some freelance web development, software development, and
-        people training.
-      </p>
-      <p>
-        His specialties in the last 5 years are often focused on developing,
-        integrating, and teaching modern software technologies. Especially
-        full-stack web application development, design, and architecture around
-        various open-source toolsets,{" "}
-        <Anchor href="https://developer.mozilla.org/JavaScript">
-          JavaScript (ES)
-        </Anchor>{" "}
-        and <Anchor href="https://typescriptlang.org">TypeScript (TS)</Anchor>{" "}
-        along with <Anchor href="https://nodejs.org">Node.js</Anchor>,{" "}
-        <Anchor href="https://deno.land">Deno</Anchor>, Express,
-        REST/GraphQL/WebSocket API, jQuery/Zepto, React/Vue/Angular, Redux,
-        MongoDB, MySQL/MariaDB, PostgreSQL, Redis, Jest/Mocha/Chai, Agile/Lean
-        Software Development with Scrum/Kanban, system administration with
-        Linux/POSIX tooling, Git source control on GitHub/GitLab/Bitbucket,
-        UI/UX design with Figma, also other programming languages such as
-        Python, Ruby, Java, Golang, and others.
-      </p>
+      <Avatar>
+        <HaidarAvatar />
+      </Avatar>
+
+      <div>
+        <p>
+          Haidar is an educator, engineer, explorer, designer, consultant, and
+          mentor. An avid remote worker and digital nomad, mostly in Asia.
+        </p>
+      </div>
+
+      <div>
+        <h2>Works</h2>
+        <p>
+          He is now focusing to develop an education venture that supports
+          distant learning and remote work named{" "}
+          <Anchor href="https://azobu.com">Azobu</Anchor>, which helps people to
+          start and grow their career in the software industry. Currently doing{" "}
+          <Anchor href="https://azobu.com/mentorship">mentorship</Anchor> there
+          as well. Furthermore, he's developing a highly-skilled professionals
+          community named{" "}
+          <Anchor href="https://highskillmasters.com">
+            High Skill Masters
+          </Anchor>
+          .
+        </p>
+        <p>
+          He was founding a coding school named{" "}
+          <Anchor href="https://impactbyte.com/about">Impact Byte</Anchor> and
+          worked with a coding school named{" "}
+          <Anchor href="https://hacktiv8.com">Hacktiv8</Anchor>. Previously, he
+          was a software engineer in{" "}
+          <Anchor href="https://agnium.co.id">Agnium</Anchor> and{" "}
+          <Anchor href="https://amp.asia">AMPlified</Anchor>. Along the way, he
+          was also doing some freelance projects of web development, software
+          development, and people training.
+        </p>
+      </div>
+
+      <div>
+        <h2>Specialties</h2>
+        <p>
+          Most often called a "Full Stack Developer" today. His specialties in
+          the last 5 years are often focused on developing, integrating, and
+          teaching modern software technologies. Especially full-stack web
+          application development, design, and architecture around various
+          open-source toolsets, namely:
+          <SpecialtyLinks></SpecialtyLinks>
+        </p>
+      </div>
+
+      <div>
+        <p>
+          If you have any questions, contact me through the{" "}
+          <Link to="/contact">contact</Link> page.
+        </p>
+      </div>
     </SectionContent>
   </Layout>
 )
