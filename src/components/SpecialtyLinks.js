c488b8cd86091416ff54289f406e8a4373a8b189
M Haidar Hanif <git@mhaidarhanif.com>
2020-07-13 21:36:42 +0700

    Separate specialty links into its own component

---
+++ src/components/SpecialtyLinks.js
@@ -0,0 +1,146 @@
+import React from "react"
+import Anchor from "../components/Anchor"
+
+export default function SpecialtyLinks() {
+  return (
+    <ol>
+      <li>
+        <Anchor href="https://developer.mozilla.org/JavaScript">
+          JavaScript (ES)
+        </Anchor>{" "}
+        and{" "}
+        <Anchor href="https://www.typescriptlang.org">TypeScript (TS)</Anchor>.
+      </li>
+      <li>
+        <Anchor href="https://nodejs.org">Node.js</Anchor> and{" "}
+        <Anchor href="https://www.npmjs.com">npm</Anchor> modules. Also (might
+        be) <Anchor href="https://deno.land">Deno</Anchor>.
+      </li>
+      <li>
+        <Anchor href="https://developer.mozilla.org/HTML">HTML</Anchor> and{" "}
+        <Anchor href="https://developer.mozilla.org/CSS">CSS</Anchor>. Some
+        templating engines and frameworks too.
+      </li>
+      <li>
+        Code editors such as{" "}
+        <Anchor href="https://code.visualstudio.com">VS Code</Anchor> and{" "}
+        <Anchor href="https://www.vim.org">Vim</Anchor>/
+        <Anchor href="https://neovim.io">Neovim</Anchor>.
+      </li>
+      <li>
+        UI/UX design with various design tools, especially{" "}
+        <Anchor href="https://www.figma.com">Figma</Anchor>. Did with{" "}
+        <Anchor href="https://www.adobe.com/sea/products/xd.html">
+          Adobe XD
+        </Anchor>{" "}
+        and <Anchor href="https://www.sketch.com">Sketch</Anchor> too.
+      </li>
+      <li>
+        Various design systems, especially{" "}
+        <Anchor href="https://getbootstrap.com">Bootstrap</Anchor> and{" "}
+        <Anchor href="https://material.io">Material Design</Anchor>.
+      </li>
+      <li>
+        <Anchor href="https://git-scm.com">Git</Anchor> source control with{" "}
+        <Anchor href="https://github.com">GitHub</Anchor>,{" "}
+        <Anchor href="https://about.gitlab.com">GitLab</Anchor>, or{" "}
+        <Anchor href="https://bitbucket.org">Bitbucket</Anchor>.
+      </li>
+      <li>
+        <Anchor href="https://en.wikipedia.org/wiki/Representational_state_transfer">
+          REST API
+        </Anchor>
+        , <Anchor href="https://graphql.org">GraphQL API</Anchor>, and{" "}
+        <Anchor href="https://en.wikipedia.org/wiki/WebSocket">
+          WebSocket API
+        </Anchor>
+        .
+      </li>
+      <li>
+        <Anchor href="https://jquery.com">jQuery</Anchor> and{" "}
+        <Anchor href="https://zeptojs.com">Zepto</Anchor> with their plugins.
+      </li>
+      <li>
+        <Anchor href="https://reactjs.org">React</Anchor> and its supporting
+        libraries, especially{" "}
+        <Anchor href="https://reactrouter.com">React Router</Anchor>. Also
+        (might be) <Anchor href="https://vuejs.org">Vue</Anchor>.
+      </li>
+      <li>
+        <Anchor href="https://redux.js.org">Redux</Anchor> and its middlewares,
+        also bindings with{" "}
+        <Anchor href="https://react-redux.js.org">React Redux</Anchor>.
+      </li>
+      <li>
+        <Anchor href="https://expressjs.com">Express</Anchor> and its
+        middlewares.
+      </li>
+      <li>
+        <Anchor href="https://www.mongodb.com">MongoDB</Anchor> with{" "}
+        <Anchor href="https://mongoosejs.com">Mongoose ODM</Anchor>.
+      </li>
+      <li>
+        <Anchor href="https://www.mysql.com">MySQL</Anchor>/
+        <Anchor href="https://mariadb.org">MariaDB</Anchor> and{" "}
+        <Anchor href="https://www.postgresql.org">PostgreSQL</Anchor> with{" "}
+        <Anchor href="https://sequelize.org">Sequelize ORM</Anchor>.
+      </li>
+      <li>
+        <Anchor href="https://jestjs.io/">Jest</Anchor> and other testing tools.
+      </li>
+      <li>
+        <Anchor href="https://en.wikipedia.org/wiki/Test-driven_development">
+          TDD
+        </Anchor>
+        ,{" "}
+        <Anchor href="https://en.wikipedia.org/wiki/Behavior-driven_development">
+          BDD
+        </Anchor>
+        , and other X-Driven Development practices.
+      </li>
+      <li>
+        <Anchor href="https://en.wikipedia.org/wiki/Agile_software_development">
+          Agile
+        </Anchor>{" "}
+        and{" "}
+        <Anchor href="https://en.wikipedia.org/wiki/Lean_software_development">
+          Lean
+        </Anchor>{" "}
+        Software Development with{" "}
+        <Anchor href="https://www.scrum.org">Scrum</Anchor>/
+        <Anchor href="https://en.wikipedia.org/wiki/Kanban">Kanban</Anchor>.
+      </li>
+      <li>
+        <Anchor href="https://www.ibm.com/design/thinking/">
+          Design Thinking
+        </Anchor>{" "}
+        and <Anchor href="https://www.gv.com/sprint/">Design Sprint</Anchor>.
+      </li>
+      <li>
+        System administration with{" "}
+        <Anchor href="https://www.linux.org">Linux</Anchor>/
+        <Anchor href="https://en.wikipedia.org/wiki/POSIX">POSIX</Anchor>
+        -related tooling, especially with{" "}
+        <Anchor href="https://ubuntu.com">Ubuntu</Anchor>.
+      </li>
+      <li>
+        Cloud computing services with{" "}
+        <Anchor href="https://cloud.google.com">Google Cloud Platform</Anchor>.
+        Did{" "}
+        <Anchor href="https://azure.microsoft.com/en-us/">
+          Microsoft Azure
+        </Anchor>{" "}
+        and <Anchor href="https://aws.amazon.com">Amazon Web Services</Anchor>{" "}
+        too, but didn't want to focus on them.
+      </li>
+      <li>
+        Other programming languages such as{" "}
+        <Anchor href="https://www.python.org">Python</Anchor>,{" "}
+        <Anchor href="https://www.ruby-lang.org/en/">Ruby</Anchor>,{" "}
+        <Anchor href="https://www.java.com/en/">Java</Anchor>,{" "}
+        <Anchor href="https://golang.org">Golang</Anchor>, etc.
+      </li>
+      <li>Technical career development, training, and mentorship.</li>
+    </ol>
+  )
+}
